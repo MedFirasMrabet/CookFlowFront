98f36b0d4ea2fe959b94aa4938c9d6d3be335c39
firas <firas@igem.org>
2025-02-14 16:26:30 +0100

    update angular 16

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LocalStorageManagerService } from './services/local-storage-manager.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private localStorageManagerService: LocalStorageManagerService, private router: Router) { }
 
